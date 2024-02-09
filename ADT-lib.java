f34f1dbc18932af900abbc4dd9f7f1956f565720
Andrea Cipriani <63643557+cipryyyy@users.noreply.github.com>
2024-02-09 12:18:04 +0100

    Update ADT-lib.java

--- ADT-lib.java
+++ ADT-lib.java
@@ -1,6 +1,6 @@
 package javaFile;           //package definition
 
-interface Container {       //Container interface
+interface Container {       //Common methods between ADTs
     void makeEmpty();       //Erase virtual size
     boolean isEmpty();      //Check if the container is virtually empty
     String print();
@@ -11,9 +11,9 @@ interface Stack extends Container {
     Object pop();               //use the first object (based on LIFO protocol)
 }
 interface Queue extends Container {
-    void enqueue(Object obj);   //add object to queue
+    void enQueue(Object obj);   //add object to queue
     Object getFront();          //read the first object that will be read
-    Object dequeue();           //use the first object (base on FIFO protocol)
+    Object deQueue();           //use the first object (base on FIFO protocol)
 }
 
 class FixedStack implements Stack{      //JAVA stack like
@@ -86,8 +86,8 @@ class CycleStack implements Stack {         //Memory action like
     //CLASS VARIABLES
     private final int BUFFER;           
     private int vSize;          //virtual size
-    private int stopPoint;      //stop point (where the stack stored the first element)
-    private int addPoint;       //add point (where the stack stored the last element)
+    private int stopIndex;      //stop point (where the stack stored the first element)
+    private int addIndex;       //add point (where the stack stored the last element)
     private Object[] v;
 
     //CLASS METHODS
@@ -102,7 +102,7 @@ class CycleStack implements Stack {         //Memory action like
         makeEmpty();
     }
     public String toString() {      //Datas about the stack
-        String ret = "Stack virtual size: " + vSize + "\nStack capacity: " + BUFFER + "\nType: Cycle\nPins location: " + stopPoint + " - - " + addPoint;
+        String ret = "Stack virtual size: " + vSize + "\nStack capacity: " + BUFFER + "\nType: Cycle\nPins location: " + stopIndex + " - - " + addIndex;
         return(ret);
     }
 
@@ -122,32 +122,32 @@ class CycleStack implements Stack {         //Memory action like
         return (vSize == 0);
     }
     public void makeEmpty() {           //Check line 33 for info
-        stopPoint = 0;
-        addPoint = 0;
+        stopIndex = 0;
+        addIndex = 0;
         vSize = 0;
     }
 
     //STACK METHODS
     public void push(Object obj) {      
         if (vSize == BUFFER) {              //If the stack is full modify add and end points
-            if (stopPoint == BUFFER-1) {        // if the stopPoint is at the end, it returns to the start
-                stopPoint = 0;
+            if (stopIndex == BUFFER-1) {        // if the stopIndex is at the end, it returns to the start
+                stopIndex = 0;
             } else {                            // otherwise it will just increment
-                stopPoint++;
+                stopIndex++;
             }
-            v[addPoint] = obj;                 //replace the old element
-            if (addPoint == BUFFER-1) {         //cycles the addPoint like stopPoint
-                addPoint = 0;
+            v[addIndex] = obj;                 //replace the old element
+            if (addIndex == BUFFER-1) {         //cycles the addIndex like stopIndex
+                addIndex = 0;
             } else {
-                addPoint++;
+                addIndex++;
             }                                   //NOTE: vSize doesn't change, it is already equal to BUFFER
-        } else {                        //actLike FixedArray, but addPoint cycles
-            v[addPoint] = obj;              
+        } else {                        //actLike FixedArray, but addIndex cycles
+            v[addIndex] = obj;              
             vSize++;                        
-            if (addPoint == BUFFER-1) {         //cycle part
-                addPoint = 0;
+            if (addIndex == BUFFER-1) {         //cycle part
+                addIndex = 0;
             } else {
-                addPoint++;
+                addIndex++;
             }
         }
     }
@@ -155,44 +155,40 @@ class CycleStack implements Stack {         //Memory action like
         if (vSize == 0) {                   //check if stack is empty
             throw new EmptyStackException();
         }
-        if (addPoint == 0) {        //returns the last element, if the addPoint points to zero, return the last element of array
+        if (addIndex == 0) {        //returns the last element, if the addIndex points to zero, return the last element of array
             return v[BUFFER-1];    //it would ask Array[index: -1], throwing ArrayOutOfBoundException.
         } else {
-            return v[addPoint-1];
+            return v[addIndex-1];
         }
     }
     public Object pop(){
         if (vSize == 0) {
             throw new EmptyStackException();
         }
-        if (addPoint == 0) {
-            addPoint = BUFFER - 1;
+        if (addIndex == 0) {
+            addIndex = BUFFER - 1;
         } else {
-            addPoint--;
+            addIndex--;
         }
-        Object obj = v[addPoint];
+        Object obj = v[addIndex];
         vSize--;
         return obj;
     }
 }
 
-class GrowingStack implements Stack{
+class GrowingStack implements Stack{        //If you hate the FullStackException
     //CLASS VARIABLES
-    private int BUFFER;
+    private int BUFFER;             //buffer isn't constant
     private int vSize;
     private Object[] v;
 
     //CLASS METHODS
     public GrowingStack() {         //constructor
-        BUFFER = 1024;
+        BUFFER = 128;
         v = new Object[BUFFER];
         makeEmpty();
     }
-    public GrowingStack(int buffer) {           //constructir user defined
-        BUFFER = buffer;
-        v = new Object[BUFFER];
-        makeEmpty();
-    }
+    //there is no user defined constrcutor and the buffer is reduced to save memory (if possible)
     public String toString() {              //Datas about the stack
         String ret = "Stack virtual size: " + vSize + "\nStack capacity: " + BUFFER + "\nType: Growing";
         return(ret);
@@ -247,21 +243,21 @@ class GrowingStack implements Stack{
     }
 }
 
-class FixedQueue implements Queue {
+class FixedQueue implements Queue {         //Works like a waiting queue
     //CLASS VARIABLES
     private Object[] v;
     private int vSize;
-    private int sPoint;
-    private int ePoint;
+    private int writeIndex;             //index were element will be added
+    private int readIndex;              //index were element will be read
     private final int BUFFER;
 
     //CLASS METHODS
-    public FixedQueue() {
+    public FixedQueue() {           //constructor
         BUFFER = 1024;
         v = new Object[BUFFER];
         makeEmpty();
     }
-    public FixedQueue(int buffer) {
+    public FixedQueue(int buffer) {     //constructor user defined
         BUFFER = buffer;
         v = new Object[BUFFER];
         makeEmpty();
@@ -274,8 +270,8 @@ class FixedQueue implements Queue {
     //CONTAINER METHODS
     public void makeEmpty() {
         vSize = 0;
-        sPoint = 0;
-        ePoint = 0;
+        writeIndex = 0;
+        readIndex = 0;
     }
     public boolean isEmpty() {
         return (vSize == 0);
@@ -293,45 +289,201 @@ class FixedQueue implements Queue {
     }
 
     //QUEUE METHODS
-    public void enqueue(Object obj) {
-        if (vSize == v.length) {
+    public void enQueue(Object obj) {           //add element to the queue
+        if (vSize == BUFFER) {                      //If queue is full
             throw new FullQueueException();
         }
         vSize++;
-        sPoint %= v.length;
-        v[sPoint] = obj;
-        sPoint++;
+        writeIndex %= v.length;             //If BUFFER is n size, and i go to index n(OutOfBound), i go to 0.
+        v[writeIndex] = obj;
+        writeIndex++;
+    }
+
+    public Object getFront() {                  //get the first element
+        if (vSize == 0) {
+            throw new EmptyQueueException();            //check if the queue is empty
+        }
+        readIndex %= v.length;
+        return (v[readIndex]);
+    }
+
+    public Object deQueue() {               //'pop' method of the queue
+        if (vSize == 0) {
+            throw new EmptyQueueException();
+        }
+        readIndex %= v.length;          //same logic of the enqueue method       
+        Object obj = getFront();
+        vSize--;
+        readIndex++;
+
+        return (obj);
+    }
+}
+
+class CycleQueue implements Queue {         //Careful with this one
+    //CLASS VARIABLES
+    private Object[] v;
+    private int vSize;
+    private int writeIndex;
+    private int readIndex;
+    private final int BUFFER;
+
+    //CLASS METHODS
+    public CycleQueue() {           //constructor
+        BUFFER = 1024;
+        v = new Object[BUFFER];
+        makeEmpty();
+    }
+    public CycleQueue(int buffer) {     //constructor user defined
+        BUFFER = buffer;
+        v = new Object[BUFFER];
+        makeEmpty();
+    }
+    public String toString() {              //Datas about the queue
+        String ret = "Queue virtual size: " + vSize + "\nQueue capacity: " + BUFFER + "\nType: Cycle";
+        return(ret);
+    }
+
+    //CONTAINER METHODS
+    public void makeEmpty() {
+        vSize = 0;
+        writeIndex = 0;
+        readIndex = 0;
+    }
+    public boolean isEmpty() {
+        return (vSize == 0);
+    }
+    public String print() {
+        String ret = "[";
+        for (int i = 0; i < BUFFER; i++) {
+            ret += v[i];
+            if (i != BUFFER-1) {
+                ret += ", ";
+            }
+        }
+        ret+="]";
+        return ret;
+    }
+
+    //QUEUE METHODS
+    public void enQueue(Object obj) {
+        if (vSize == BUFFER) {              //if the buffer is full, i move the readIndex
+            readIndex++;                        //in this way I have a space in the queue
+            readIndex %= BUFFER;                //obviously the first element will be erased
+            writeIndex %= BUFFER;
+            v[writeIndex] = obj;
+            writeIndex++;
+        } else {                    //else, works like the fixed queue
+            vSize++;
+            writeIndex %= BUFFER;
+            v[writeIndex] = obj;
+            writeIndex++;
+        }
     }
 
     public Object getFront() {
         if (vSize == 0) {
             throw new EmptyQueueException();
         }
-        ePoint %= v.length;
-        return (v[ePoint]);
+        readIndex %= v.length;
+        return (v[readIndex]);
     }
 
-    public Object dequeue() {
+    public Object deQueue() {
         if (vSize == 0) {
             throw new EmptyQueueException();
         }
-        ePoint %= v.length;
+        readIndex %= v.length;
         Object temp = getFront();
         vSize--;
-        ePoint++;
+        readIndex++;
+
+        return (temp);
+    }
+}
+
+class GrowingQueue {                    //ERROR WHILE DEQUEUEING, DON'T USE
+    //CLASS VARIABLES
+    private Object[] v;
+    private int vSize;
+    private int writeIndex;
+    private int readIndex;
+    private int BUFFER;             //buffer is a variable
+
+    //CLASS METHODS
+    public GrowingQueue() {           //constructor
+        BUFFER = 4;
+        v = new Object[BUFFER];
+        makeEmpty();
+    }
+    //no user defined constructor, buffer is reduced
+    public String toString() {              //Datas about the queue
+        String ret = "Queue virtual size: " + vSize + "\nQueue capacity: " + BUFFER + "\nType: Cycle";
+        return(ret);
+    }
+    private Object[] resize(Object[] arr, int actualIndex, int size) {      //RESIZE the queue
+        Object[] newArr = new Object[size];                         
+        System.arraycopy(arr, 0, newArr, 0, actualIndex);           //copies the element before the index at the start
+        System.arraycopy(arr, actualIndex, newArr, size-arr.length, arr.length-actualIndex);  //the element after at the end of the array
+        return newArr;
+    }
+
+    //CONTAINER METHODS
+    public void makeEmpty() {
+        vSize = 0;
+        writeIndex = 0;
+        readIndex = 0;
+    }
+    public boolean isEmpty() {
+        return (vSize == 0);
+    }
+    public String print() {
+        String ret = "[";
+        for (int i = 0; i < BUFFER; i++) {
+            ret += v[i];
+            if (i != BUFFER-1) {
+                ret += ", ";
+            }
+        }
+        ret+="]";
+        return ret;
+    }
+
+    //QUEUE METHODS
+    public void enQueue(Object obj) {           //add element to the queue
+        if (vSize == BUFFER) {                      //If queue is full
+            BUFFER *=2;
+            v = resize(v, writeIndex, BUFFER);
+        }
+        vSize++;
+        writeIndex %= v.length;             //If BUFFER is n size, and i go to index n(OutOfBound), i go to 0.
+        v[writeIndex] = obj;
+        writeIndex++;
+    }
+
+    public Object getFront() {
+        if (vSize == 0) {
+            throw new EmptyQueueException();
+        }
+        readIndex %= v.length;
+        return (v[readIndex]);
+    }
+
+    public Object deQueue() {           //ERROR while reading values
+        if (vSize == 0) {
+            throw new EmptyQueueException();
+        }
+        readIndex %= v.length;
+        Object temp = getFront();
+        vSize--;
+        readIndex++;
 
         return (temp);
     }
 }
 
 /*TODO:
- * class FixedQueue {
- *  
- * }
- * class CycleQueue {
- * 
- * }
- * class GrowingQueue {
+ * class FixedDeque {
  * 
  * }
  * class CycleDeque {
@@ -340,7 +492,7 @@ class FixedQueue implements Queue {
  * class GrowingDeque {
  * 
  * }
- */
+*/
 
 //EXCEPTIONS
 class FullQueueException extends RuntimeException {         //Exception thrown if the queue is full
